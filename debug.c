95551378549561a2899d9fd2db2b0e4ffd700e17
wty <623417407@qq.com>
2025-09-27 16:11:52 +0800

    update

--- debug.c
+++ debug.c
@@ -11,8 +11,7 @@ void disassembleChunk(Chunk* chunk, const char* name) {
   printf("Ofst Line Instruction         Operand\n");
   printf("---- ---- -----------         -------\n");
 
-  for (int offset = 0; offset < chunk->count;)
-  {
+  for (int offset = 0; offset < chunk->count;) {
     offset = disassembleInstruction(chunk, offset);
   }
 }
@@ -20,6 +19,12 @@ void disassembleChunk(Chunk* chunk, const char* name) {
 static int simpleInstruction(const char* name, int offset) {
   printf("%s\n", name);
   return offset + 1;
+}
+
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+  uint8_t slot = chunk->code[offset + 1];
+  printf("%-16s %4d\n", name, slot);
+  return offset + 2;
 }
 
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
@@ -46,59 +51,59 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   int preInstructionLine = getLine(&chunk->lineInfos, offset - 1);
   int thisInstructionLine = getLine(&chunk->lineInfos, offset);
 
-  if (offset > 0 && preInstructionLine == thisInstructionLine)
-  {
+  if (offset > 0 && preInstructionLine == thisInstructionLine) {
     printf("   | ");
-  }
-  else
-  {
+  } else {
     printf("%4d ", thisInstructionLine);
   }
   uint8_t instruction = chunk->code[offset];
-  switch (instruction)
-  {
-  case OP_PRINT:
-    return simpleInstruction("OP_PRINT", offset);
-  case OP_RETURN:
-    return simpleInstruction("OP_RETURN", offset);
-  case OP_CONSTANT:
-    return constantInstruction("OP_CONSTANT", chunk, offset);
-  case OP_CONSTANT_LONG:
-    return constantInstructionLong("OP_CONSTANT_LONG", chunk, offset);
-  case OP_NEGATE:
-    return simpleInstruction("OP_NEGATE", offset);
-  case OP_ADD:
-    return simpleInstruction("OP_ADD", offset);
-  case OP_SUBTRACT:
-    return simpleInstruction("OP_SUBTRACT", offset);
-  case OP_MULTIPLY:
-    return simpleInstruction("OP_MULTIPLY", offset);
-  case OP_DIVIDE:
-    return simpleInstruction("OP_DIVIDE", offset);
-  case OP_NOT:
-    return simpleInstruction("OP_NOT", offset);
-  case OP_NIL:
-    return simpleInstruction("OP_NIL", offset);
-  case OP_TRUE:
-    return simpleInstruction("OP_TRUE", offset);
-  case OP_FALSE:
-    return simpleInstruction("OP_FALSE", offset);
-  case OP_SET_GLOBAL:
-    return constantInstruction("OP_SET_GLOBAL", chunk, offset);
-  case OP_GET_GLOBAL:
-    return constantInstruction("OP_GET_GLOBAL", chunk, offset);
-  case OP_DEFINE_GLOBAL:
-    return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
-  case OP_EQUAL:
-    return simpleInstruction("OP_EQUAL", offset);
-  case OP_GREATER:
-    return simpleInstruction("OP_GREATER", offset);
-  case OP_LESS:
-    return simpleInstruction("OP_LESS", offset);
-  case OP_POP:
-    return simpleInstruction("OP_POP", offset);
-  default:
-    printf("Unknown opcode %d\n", instruction);
-    return offset + 1;
+  switch (instruction) {
+    case OP_PRINT:
+      return simpleInstruction("OP_PRINT", offset);
+    case OP_RETURN:
+      return simpleInstruction("OP_RETURN", offset);
+    case OP_CONSTANT:
+      return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_CONSTANT_LONG:
+      return constantInstructionLong("OP_CONSTANT_LONG", chunk, offset);
+    case OP_NEGATE:
+      return simpleInstruction("OP_NEGATE", offset);
+    case OP_ADD:
+      return simpleInstruction("OP_ADD", offset);
+    case OP_SUBTRACT:
+      return simpleInstruction("OP_SUBTRACT", offset);
+    case OP_MULTIPLY:
+      return simpleInstruction("OP_MULTIPLY", offset);
+    case OP_DIVIDE:
+      return simpleInstruction("OP_DIVIDE", offset);
+    case OP_NOT:
+      return simpleInstruction("OP_NOT", offset);
+    case OP_NIL:
+      return simpleInstruction("OP_NIL", offset);
+    case OP_TRUE:
+      return simpleInstruction("OP_TRUE", offset);
+    case OP_FALSE:
+      return simpleInstruction("OP_FALSE", offset);
+    case OP_GET_LOCAL:
+      return byteInstruction("OP_GET_LOCAL", chunk, offset);
+    case OP_SET_LOCAL:
+      return byteInstruction("OP_SET_LOCAL", chunk, offset);
+    case OP_SET_GLOBAL:
+      return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+    case OP_GET_GLOBAL:
+      return constantInstruction("OP_GET_GLOBAL", chunk, offset);
+    case OP_DEFINE_GLOBAL:
+      return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
+    case OP_EQUAL:
+      return simpleInstruction("OP_EQUAL", offset);
+    case OP_GREATER:
+      return simpleInstruction("OP_GREATER", offset);
+    case OP_LESS:
+      return simpleInstruction("OP_LESS", offset);
+    case OP_POP:
+      return simpleInstruction("OP_POP", offset);
+    default:
+      printf("Unknown opcode %d\n", instruction);
+      return offset + 1;
   }
 }
